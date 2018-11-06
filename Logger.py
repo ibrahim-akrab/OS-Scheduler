858167a93c8e3812da2a145260d38d03656dc53d
Ibrahim-Akrab <ibrahim.m.akrab@gmail.com>
2018-11-07 00:07:47 +0200

    add logging arrival times and runtime into logger

--- Logger.py
+++ Logger.py
@@ -1,17 +1,45 @@
+
+
 class Logger:
 
     def __init__(self):
-        # list of the times with the processes running in that time
-        self.runtime = [[0], [0]]
-        self.arrival = [[], []]
-        self.waiting = [[], []]
+        # list of the times with the processes running in that time to be plotted
+        self.plotting_data = [[0], [0]]
+        self.arrival = {}
+        self.running = {}
+        self.waiting_time = {}
+        self.start_running_time = 0
 
     def log_runtime(self, process, clock, starting=True):
+
         if clock.time is 0:
-            self.runtime[1].pop()
-        self.runtime[0].append(clock.time)
+            self.plotting_data[1].pop()
+        self.plotting_data[0].append(clock.time)
         if starting:
-            self.runtime[1].append(process.process_number)
+            self.start_running_time = clock.time
+            self.arrived(process, clock, arriving=False)
+            self.plotting_data[1].append(process.process_number)
         else:
-            self.runtime[1].append(0)
+            if process.process_number in self.running:
+                self.running[process.process_number] += clock.time - self.start_running_time
+            else:
+                self.running[process.process_number] = clock.time - self.start_running_time
+            self.plotting_data[1].append(0)
+            if process.burst_time is not 0:
+                self.arrived(process, clock, arriving=True)
+
+    def arrived(self, process, clock, arriving=True):
+        if arriving:
+            self.arrival[process.process_number] = clock.time
+        else:
+            if process.process_number in self.waiting_time:
+                self.waiting_time[process.process_number] += clock.time - self.arrival[process.process_number]
+            else:
+                self.waiting_time[process.process_number] = clock.time - self.arrival[process.process_number]
+            self.arrival[process.process_number] = 0
+
+    # def write_log(self, output_file):
+    #     sorted_waiting = dict(sorted(self.waiting_time.items()))
+    #     sorted_runtime = dict(sorted(self.running))
+    #     with open(output_file) as output:
 
