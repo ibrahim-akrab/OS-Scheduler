6b0fed99f7d29fbf6dd91fa93ff64b5956118a87
m7mdemad <omdaemad28@yahoo.com>
2018-11-06 22:14:43 +0200

    nothing

--- fcfs.py
+++ fcfs.py
@@ -1,47 +1,31 @@
-import numpy as np
-from readProcessesFile import readProcessesFile
-from writeresults import writeResults
-
-def FCFS(): #suppose to take inputfile from the GUI
-    #intialize list to gethe data from the file in
-    arrival = []
-    burst = []
-    priority = []
-    number_of_processes = 0
-    inputfile='output.txt' #just for now
-    number_of_processes = readProcessesFile(inputfile,arrival,burst,priority)
-
-    #print(number_of_processes)
-    #print(arrival)
-    #print(burst)
-    #print(priority)
+from Scheduler import Scheduler, SchedulerState
 
 
-    wt=[] #list of waiting time for each process
-    tat=[] #list of turnaround time for each process
-    wtat=[] #list of weighted turnaround time for each process
+class FCFS(Scheduler):
 
-    #calculate waiting time for each processe
-    wt.append(0)
-    for i in range(1,number_of_processes):
-        w = 0
-        for j in range(i):
-            w += burst[j]
-        wt.append(w)
+    def process_arrived(self, process):
+        print(self.clock, "arrived", process)
+        self.processes.append(process)
+        if len(self.processes) is 1:
+            self.run()
 
-    #calculate turnaround time for each processe, avg turnaround and avg weighted turn around
-    avg_tat = 0
-    avg_wtat = 0
-    for i in range(number_of_processes):
-        tat.append(burst[i] + wt[i])
-        wtat.append(tat[i]/burst[i]) #not sure
-        avg_tat += tat[i]
-        avg_wtat += wtat[i]
+    def notify(self):
+        if self.state is SchedulerState.running:
+            process = self.processes.pop(0)
+            print(self.clock, "finished running", process)
+            self.state = SchedulerState.context_switching
+            print(self.clock, "started context switching")
+            self.clock.notify_scheduler(self.context_switching)
+        elif self.state is SchedulerState.context_switching:
+            self.state = None
+            print(self.clock, "finished context switching")
+            if len(self.processes) is not 0:
+                self.run()
 
-    avg_tat /= number_of_processes
-    avg_wtat /= number_of_processes
+    def run(self):
+        process = self.processes[0]
+        print(self.clock, "started running", process)
+        self.clock.notify_scheduler(process.burst_time)
+        self.state = SchedulerState.running
+        process.run(process.burst_time)
 
-    writeResults(arrival, wt, tat, wtat, avg_tat, avg_wtat, number_of_processes)
-
-
-FCFS()
